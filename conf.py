e82f2d22d754d683e7a260f2d50fb6f8cf297991
Alexander Kostetsky <finster.seele@gmail.com>
2018-02-03 23:48:13 +0300

    Nikola auto commit.
    Nikola version: 7.8.11

--- conf.py
+++ conf.py
@@ -989,7 +989,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "alexanderkostetsky"
+COMMENT_SYSTEM_ID = "alexanderkostetskiy"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
